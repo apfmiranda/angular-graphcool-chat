3ce9f8ec60a7d57687fb9f7f0f13202318d37b22
apfmiranda <alexandre.miranda@gmail.com>
2019-05-21 20:59:32 -0300

    Método no ChatService para buscar lista de chats

--- src/app/chat/services/chat.service.ts
+++ src/app/chat/services/chat.service.ts
@@ -1,10 +1,32 @@
-import { ChatModule } from './../chat.module';
+import { Apollo } from 'apollo-angular';
+import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
+
+import { AllChatsQuery, USER_CHATS_QUERY } from './chat.graphql';
+import { AuthService } from './../../core/services/auth.service';
+import { Chat } from '../models/chat.model';
+import { ChatModule } from './../chat.module';
 
 @Injectable({
   providedIn: ChatModule
 })
 export class ChatService {
 
-  constructor() { }
+  constructor(
+    private apollo: Apollo,
+    private authService: AuthService
+  ) { }
+
+  getUserChats(): Observable<Chat[]> {
+    return this.apollo
+      .query<AllChatsQuery>({
+        query: USER_CHATS_QUERY,
+        variables: {
+          userId: this.authService.authUser.id
+        }
+      }).pipe(
+        map(res => res.data.allChats)
+      );
+  }
 }
